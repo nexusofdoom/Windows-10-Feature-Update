61ae0fa63f569a7f1a0f1070ef9e2deae787e0fb
nexusofdoom <travus@discoverpc.net>
2022-05-03 12:35:00 -0500

    Create Update.bat

---
+++ Update.bat
@@ -0,0 +1,5 @@
+powershell Set-ExecutionPolicy unrestricted
+powershell curl.exe ""
+powershell .\C:\Windows\Temp\Windows_10_21H2_X64_Update_Script.ps1
+powershell Set-ExecutionPolicy  restricted
+del C:\Windows\Temp\Windows_10_21H2_X64_Update_Script.ps1
