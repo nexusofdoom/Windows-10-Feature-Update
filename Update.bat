74ab9dc698faa7afe19b667573344eaffef9f587
nexusofdoom <travus@discoverpc.net>
2022-05-03 13:08:59 -0500

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,6 +1,7 @@
 powershell Set-ExecutionPolicy unrestricted
 powershell curl.exe -LJ https://raw.githubusercontent.com/nexusofdoom/Windows-10-Feature-Update/main/Windows_10_X64_Update_Script.ps1 -o C:\Windows\Temp\Windows_10_X64_Update_Script.ps1
-powershell .\C:\Windows\Temp\Windows_10_X64_Update_Script.ps1
+cd C:\Windows\Temp
+powershell .\Windows_10_X64_Update_Script.ps1
 powershell Set-ExecutionPolicy  restricted
 del C:\Windows\Temp\Windows_10_X64_Update_Script.ps1
 del C:\Windows\Temp\Update.bat
